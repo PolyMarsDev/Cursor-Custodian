572aa86c44a4824e7928f6f0c9e468a34ddb9272
Christian Himmler <christian@himmlers.de>
2020-12-28 01:17:20 +0100

    Added CMake, Fixed Memory Leak, Fixed Score

--- include/ground.h
+++ include/ground.h
@@ -1,6 +1,6 @@
 #pragma once
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
+#include <SDL.h>
+#include <SDL_image.h>
 #include <vector>
 
 #include "groundtile.h"
--- src/entity.cpp
+++ src/entity.cpp
@@ -1,11 +1,11 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
+#include <SDL.h>
+#include <SDL_image.h>
 #include <vector>
 
 #include "entity.h"
 
-Entity::Entity(float p_x, float p_y, std::vector<SDL_Texture*> p_tex)
-:x(p_x), y(p_y), tex(p_tex)
+Entity::Entity(float p_x, float p_y, std::vector<SDL_Texture *> p_tex)
+	: x(p_x), y(p_y), tex(p_tex)
 {
 	currentFrame.x = 0;
 	currentFrame.y = 0;
@@ -17,8 +17,8 @@ Entity::Entity(float p_x, float p_y, std::vector<SDL_Texture*> p_tex)
 	}
 }
 
-Entity::Entity(float p_x, float p_y, SDL_Texture* p_tex)
-:x(p_x), y(p_y)
+Entity::Entity(float p_x, float p_y, SDL_Texture *p_tex)
+	: x(p_x), y(p_y)
 {
 	tex.push_back(p_tex);
 	currentFrame.x = 0;
@@ -86,7 +86,7 @@ void Entity::setAnimOffsetY(int p_index, int p_value)
 	animOffsetsY[p_index] = p_value;
 }
 
-SDL_Texture* Entity::getTex(int p_index)
+SDL_Texture *Entity::getTex(int p_index)
 {
 	return tex.at(p_index);
 }
@@ -96,7 +96,7 @@ SDL_Rect Entity::getCurrentFrame()
 	return currentFrame;
 }
 
-void Entity::setTex(SDL_Texture* p_tex)
+void Entity::setTex(SDL_Texture *p_tex)
 {
 	tex[0] = p_tex;
 }
